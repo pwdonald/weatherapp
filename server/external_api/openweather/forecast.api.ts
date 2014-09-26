b0c0b4767df9d3dadab0de6a9ca2392a44f74fd9
Donald Jones <donald@getplatypi.com>
2014-09-26 18:14:13 -0500

    Started work on forecast api methods.

---
+++ server/external_api/openweather/forecast.api.ts
@@ -0,0 +1,10 @@
+﻿/// <reference path="../../typings/tsd.d.ts" />
+/// <reference path="../../_references.ts" />
+
+import OpenWeatherApi = require('./openweather.api');
+
+class ForecastApi extends OpenWeatherApi {
+    
+}
+
+export = ForecastApi;
