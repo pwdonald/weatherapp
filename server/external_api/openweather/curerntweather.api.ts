11530d8288f28739759bd6875e00a73f2297ccd4
Donald Jones <donald@getplatypi.com>
2014-09-26 18:13:00 -0500

    Implemented Current Weather API methods.

--- server/external_api/openweather/curerntweather.api.ts
+++ server/external_api/openweather/curerntweather.api.ts
@@ -1,10 +1,33 @@
 ﻿/// <reference path="../../typings/tsd.d.ts" />
+/// <reference path="../../_references.ts" />
 
 import OpenWeatherApi = require('./openweather.api');
 
-
 class CurrentWeatherApi extends OpenWeatherApi {
+    getWeatherByCityName = (apiToken: string, cityName: string): Thenable<ICurrentWeather> => {
+        var query = {
+            q: cityName
+        };
 
+        return this.get('weather', query, apiToken);
+    };
+
+    getWeatherByCoordinates = (apiToken: string, lat: number, lon: number): Thenable<ICurrentWeather> => {
+        var query = {
+            lat: lat,
+            lon: lon
+        };
+
+        return this.get('weather', query, apiToken);
+    };
+
+    getWeatherByCityId = (apiToken: string, cityId: number): Thenable<ICurrentWeather> => {
+        var query = {
+            id: cityId
+        };
+
+        return this.get('weather', query, apiToken);
+    };
 }
 
 export = CurrentWeatherApi;
