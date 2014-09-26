1b1491a1f04773435a9f119b1fda101cfaf1c8bb
Donald Jones <donald@getplatypi.com>
2014-09-26 11:44:08 -0500

    implemented a base api that will limit the number of requests.

--- weatherapp/server/external_api/openweather/base.api.ts
+++ weatherapp/server/external_api/openweather/base.api.ts
@@ -1,1 +1,48 @@
-﻿ 
+﻿/// <reference path="../../typings/tsd.d.ts" />
+
+import request = require('request');
+import https = require('https');
+import PromiseStatic = require('es6-promise');
+import qs = require('querystring');
+import utils = require('../../utils/utils');
+
+/*
+ * All API calls should go through this module to prevent too many calls being made per minute.
+ */
+var callCount = 0,
+    Promise = PromiseStatic.Promise,
+    requestQueue: Array<Promise<any>> = [];
+
+class BaseApi {
+    static getApiCall = (url: string, querystr?: Object, token?: string): Thenable<any> => {
+        if (callCount < 3000) {
+            return BaseApi.get(url, querystr, token);
+        } else {
+            requestQueue.push(BaseApi.get.bind(null, url, querystr, token)); 
+
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    callCount = 0;
+                    resolve();
+                }, 60000);
+            }).then(() => {
+                return Promise.all(requestQueue);
+            });
+        }
+    };
+
+    static get = (url: string, querystr?: Object, token?: string): Thenable<any> => {
+        querystr['APPID'] = (!utils.isNull(token) ? token : null);
+        url += qs.stringify(querystr);
+        return new Promise((resolve, reject) => {
+            request.get(url, (err, res, body) => {
+                if (utils.isObject(err)) {
+                    return reject(err);
+                }
+                resolve(body);
+            });
+        });
+    };
+}
+
+export = BaseApi;
