30901ca1f98a68da4ed3b7d61ee45ddb72a240e6
Donald Jones <donald@getplatypi.com>
2014-09-29 17:17:05 -0500

    Added temp interface.

---
+++ server/external_api/openweather/interfaces/main/temp.interface.ts
@@ -0,0 +1,9 @@
+﻿/// <reference path="../openweather.interfaces.ts" />
+interface ITemp {
+    day?: number;
+    min?: number;
+    max?: number;
+    night?: number;
+    even?: number;
+    morn?: number;
+} 
